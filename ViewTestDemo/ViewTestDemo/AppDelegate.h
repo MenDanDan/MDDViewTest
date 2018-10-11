055b9cf72c6d721b662fd9ad5ab0538d11f9007b
lihaichao <lihaichao@qq.com>
2018-10-11 15:27:00 +0800

    viewTestDemo初始化

---
+++ ViewTestDemo/ViewTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewTestDemo
+//
+//  Created by 55556 on 2018/10/11.
+//  Copyright © 2018年 55556. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewTestDemo/ViewTestDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ViewTestDemo
+//
+//  Created by 55556 on 2018/10/11.
+//  Copyright © 2018年 55556. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
